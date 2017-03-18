03748a8dcd0f2d6dabe736738cffa43a9a4f6c7b
1ZouLTReX1 <amir.trex@gmail.com>
2017-03-18 13:31:54 +0200

    couple name changes + score added

--- sketch.js
+++ sketch.js
@@ -3,22 +3,42 @@ var obs = [];
 var onoff = false;
 var tempo = 150;
 var start = Math.round(new Date() / 10);
+var time = 0;
+var bestTime = 0;
 var lives = 5;
-var score = 0;
+var score;
+var best;
 
 function setup() {
   createCanvas(innerHeight, innerHeight);
+	score = createP();
+	best = createP();
+	score.html("Score: 0");
+	best.html("Best: 0");
   worm = new Player(random(width/4,width*0.75), random(height/4,height*0.75)); 
 	obs.push(new Obsticle(floor(random(4))));	
   var attractor = createVector(width/2, height/2);  
 	var now = Math.round(new Date() / 10);
 }
 
+function reset() {
+	worm.history = []
+	worm.pos.x = random(width/4,width*0.75);
+	worm.pos.y = random(height/4,height*0.75);
+	obs = []
+	frameCount = 0;
+	lives = 5;
+	time = 0;
+	start = Math.round(new Date() / 10);
+}
+
 function draw() {
   background(50);
 	now = Math.round(new Date() / 10);
-	score = now - start;
-	console.log(score/100);
+	time = (now - start) / 100;
+	bestTime = Math.max(bestTime, time);
+	score.html("<p class = score> Score: " + time + "</p>");
+	best.html("<p class = best> Best: " + bestTime + "</p>");
 	
   // Attractor
   attractor = createVector(mouseX, mouseY);
@@ -73,10 +93,10 @@ function draw() {
 	
 	// Attractor
 	// Draws the attractor.	
-  strokeWeight(9);
+  strokeWeight(10);
   stroke(0, 25 , 255);
   point(attractor.x, attractor.y);
-	strokeWeight(4);
+	strokeWeight(6);
   stroke(250);
 	point(attractor.x, attractor.y);
 	
